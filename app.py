9356e2d4f0c3f15d19a09ea1ef024f44e475e6cc
wtoth <wtoth@umich.edu>
2020-04-18 12:25:55 -0400

    Addeed new queries

--- app.py
+++ app.py
@@ -51,5 +51,102 @@ def addPatient():
         return 'success'
     return render_template('addPatient.html')
 
+#Works
+@app.route('/emergency-contact', methods=['GET', 'POST'])
+def getEmergencyContact():
+    if request.method == "POST":
+        details = request.form
+
+        #Save Values to Variables
+        ID = details['ID']
+
+        #Connect to DB
+        cur = mysql.connection.cursor()
+
+        #Execute Query
+        cur.execute("SELECT contact_name, Phone_number, Relationship From emergency_contact where  patient_id = %s", (ID))
+        
+        data = cur.fetchall()
+        #Commit Query through connection
+        mysql.connection.commit()
+        
+        #Close DB Connection
+        cur.close()
+        return str(data)
+    return render_template('emergencyContact.html')
+
+#Works
+@app.route('/admittance-date', methods=['GET', 'POST'])
+def admitDate():
+    if request.method == "POST":
+        details = request.form
+
+        #Save Values to Variables
+        ID = details['ID']
+
+        #Connect to DB
+        cur = mysql.connection.cursor()
+
+        #Execute Query
+        cur.execute("select Date_admitted from patient where  patient_id = %s", (ID))
+        
+        data = cur.fetchall()
+        #Commit Query through connection
+        mysql.connection.commit()
+        
+        #Close DB Connection
+        cur.close()
+        return str(data)
+    return render_template('admitDate.html')
+
+#Works
+@app.route('/patient-info', methods=['GET', 'POST'])
+def patientInfo():
+    if request.method == "POST":
+        details = request.form
+
+        #Save Values to Variables
+        ID = details['ID']
+
+        #Connect to DB
+        cur = mysql.connection.cursor()
+
+        #Execute Query
+        cur.execute("SELECT s.Name, t.Diagnosis, t.prescribed_treatment From treats t, patient p, staff s where  p.Patient_id = %s AND p.Patient_id = t.Patient_id AND t.Doctor_id = s.Employee_id;", (ID))
+        
+        data = cur.fetchall()
+        #Commit Query through connection
+        mysql.connection.commit()
+        
+        #Close DB Connection
+        cur.close()
+        return str(data)
+    return render_template('getInfo.html')
+
+
+@app.route('/delete-patient', methods=['GET', 'POST'])
+def deletePatient():
+    if request.method == "POST":
+        details = request.form
+
+        #Save Values to Variables
+        ID = details['ID']
+
+        #Connect to DB
+        cur = mysql.connection.cursor()
+
+        #Execute Query
+        cur.execute("DELETE FROM `cis421`.`emergency_contact` WHERE Patient_id = %s;", (ID))
+        cur.execute("DELETE FROM `cis421`.`treats` WHERE Patient_id = %s;", (ID))
+        cur.execute("DELETE FROM `cis421`.`patient` WHERE Patient_id = %s;", (ID))
+    
+        #Commit Query through connection
+        mysql.connection.commit()
+        
+        #Close DB Connection
+        cur.close()
+        return 'Patient Deleted'
+    return render_template('deletePatient.html')
+
 if __name__ == '__main__':
     app.run()
